59a4183b95ab8de5be3ad85504f36ea81e15ff44
borisov <hoppano@mail.ru>
2020-09-13 17:47:28 +0300

    pr 1_2

--- src/PR_1_2.kt
+++ src/PR_1_2.kt
@@ -1,18 +1,30 @@
+@file:Suppress("DEPRECATED_IDENTITY_EQUALS")
 /* 5. В строке указано несколько неотрицательных целых чисел,
 разделенных пробелами (по одному пробелу между числами).
 Какие цифры присутствуют ровно в двух числах? */
 
 fun main () {
     println("Введите неотрицательные числа через пробел")
-    val input = readLine()
+    val input: String? = readLine()
+    val numbers: Array<Int> = Array(size = 10) {0}
+    var uniqueNumbers: Array<Int> = Array(size = 10) {0}
+    var indexedNumber: Int = 0
+
     if (input == null || input == "") {
         println("Ошибка! Введите числа через пробел")
         return
     }
 
-    for (num in input) {
-        
+    for (char: Char in input) {
+        if (char != ' ') {
+            uniqueNumbers[char.toInt() - '0'.toInt()]++
+            if (uniqueNumbers[char.toInt() - '0'.toInt()] == 1) numbers[char.toInt() - '0'.toInt()]++
+        }
+        if (char == ' ') uniqueNumbers = Array(size = 10) {0}
     }
 
-
+    for (quantity in numbers) {
+        if (quantity == 2) println(indexedNumber)
+        indexedNumber ++
+    }
 }
